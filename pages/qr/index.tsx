8e85e52b93336bfe167b628b4f8af8090dd55d8d
walle233 <2310005663@qq.com>
2024-04-05 10:44:18 +0800

    test qr code

--- pages/qr/index.tsx
+++ pages/qr/index.tsx
@@ -1,30 +1,44 @@
-import React, { useState } from 'react';
-import { QrReader } from 'react-qr-reader';
+// import React, { useState } from 'react';
+// import { QrReader } from 'react-qr-reader';
 
-const Test = (props:any) => {
-  const [data, setData] = useState('No result');
+// const Test = (props:any) => {
+//   const [data, setData] = useState('No result');
 
-  return (
-    <>
-      <QrReader
-        onResult={(result: any, error) => {
-          if (!!result) {
-            console.info(result);
-            setData(result?.text);
-          }
+//   return (
+//     <>
+//       <QrReader
+//         onResult={(result: any, error) => {
+//           if (!!result) {
+//             console.info(result);
+//             setData(result?.text);
+//           }
 
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        constraints={{}}
-      />
-      <p>{data}</p>
-    </>
-  );
-
-
-};
+//           if (!!error) {
+//             console.info(error);
+//           }
+//         }}
+//         constraints={{}}
+//       />
+//       <p>{data}</p>
+//     </>
+//   );
 
 
-export default Test;
+// };
+
+
+// export default Test;
+
+
+import { Scanner } from '@yudiel/react-qr-scanner';
+
+const App = () => {
+    return (
+        <Scanner
+            onResult={(text, result) => console.log(text, result)}
+            onError={(error) => console.log(error?.message)}
+        />
+    );
+}
+
+export default App;
